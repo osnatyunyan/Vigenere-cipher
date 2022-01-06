e43fca38d22100b3b07130678e976578d8d441ed
Osnat Yunyan <83605501+osnatyunyan@users.noreply.github.com>
2022-01-06 22:59:00 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+def main():
+    """The program will implement writing and decoding a code for text files that contain Latin letters"""
+    number_use= input("Please choose between e and d-  ")
+    if number_use=="e":
+        key=input("please write Encryption key- ")
+        file=input("please write  name of file- ")
+        print(e(key,file))
+    if number_use=="d":
+        key=input("please write Encryption key- ")
+        file=input("please write name of file- ")
+        print (d(key,file))
