e893d6c0f44a80f8cf7589dfe8bcbc67b0845ee9
Osnat Yunyan <83605501+osnatyunyan@users.noreply.github.com>
2022-01-06 22:59:50 +0200

    Update vigenere.py

--- vigenere.py
+++ vigenere.py
@@ -98,18 +98,6 @@ def d(key,file):
     f.close()
     return (vigenere_decrypt(s,key))   
     
-def main():
-    """The program will implement writing and decoding a code for text files that contain Latin letters"""
-    number_use= input("Please choose between e and d-  ")
-    if number_use=="e":
-        key=input("please write Encryption key- ")
-        file=input("please write  name of file- ")
-        print(e(key,file))
-    if number_use=="d":
-        key=input("please write Encryption key- ")
-        file=input("please write name of file- ")
-        print (d(key,file))
-main()
         
         
         
